0e91393e61bf003779c4a7c383d24b81c38f8402
doronhi <40540281+doronhi@users.noreply.github.com>
2018-08-29 15:49:16 +0300

    Revert "build with librealsense 2.16"

--- realsense2_camera/include/base_realsense_node.h
+++ realsense2_camera/include/base_realsense_node.h
@@ -51,15 +51,6 @@ namespace realsense2_camera
     };
     typedef std::pair<image_transport::Publisher, std::shared_ptr<FrequencyDiagnostics>> ImagePublisherWithFrequencyDiagnostics;
 
-	class PipelineSyncer : public rs2::asynchronous_syncer
-	{
-	public: 
-		void operator()(rs2::frame f) const
-		{
-			invoke(std::move(f));
-		}
-	};
-
     class BaseRealSenseNode : public InterfaceRealSenseNode
     {
     public:
@@ -169,7 +160,7 @@ namespace realsense2_camera
         bool _align_depth;
         bool _sync_frames;
         bool _pointcloud;
-		PipelineSyncer _syncer;
+        rs2::asynchronous_syncer _syncer;
 
         std::map<stream_index_pair, cv::Mat> _depth_aligned_image;
         std::map<stream_index_pair, std::string> _depth_aligned_encoding;
